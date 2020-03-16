f5386fee65aaabc01ee09d10389ac8787e2e0ae7
slavabily <slavabily@me.com>
2020-03-16 14:53:47 +0200

    app core functionality is finished

--- Multiplication/ContentView.swift
+++ Multiplication/ContentView.swift
@@ -16,23 +16,22 @@ struct Question {
     var question: String {
         return "What is \(tableNumber) x \(multiplier) ?"
     }
-    
-    var answer: Int {
-        return tableNumber * multiplier
+    var answer: String {
+        return String(tableNumber * multiplier)
     }
 }
 
 struct ContentView: View {
     
     @State var questions = [String]()
-    @State var answers = [Int]()
+    @State var answers = [String]()
     
     var howManyQuestions = ["5", "10", "20", "All"]
     @State private var selectedQuestQuantity = 0
     
     var selectedQuestionsLimit: Int {
         let selected = howManyQuestions[selectedQuestQuantity]
-        return Int(selected) ?? 0
+        return Int(selected) ?? 30
     }
     
     @State private var remainingQuestionsQuantity = 0
@@ -76,10 +75,7 @@ struct ContentView: View {
             Form {
                 if isGameRunning == true {
                     Text("Question: \(questions[currentQuestionNumber])")
-                } else {
-                    Text("Question: ")
-                }
-                if isGameRunning == true {
+                    
                     TextField("Answer:", text: $answer) {
                         self.acceptAnswer(self.answer)
                         self.answer = ""
@@ -90,11 +86,15 @@ struct ContentView: View {
     }
     
     func start() {
+        questions.removeAll()
+        answers.removeAll()
         
-        for i in 1 ... 10 {
-            let question = Question(tableNumber: table, multiplier: i)
-            questions.append(question.question)
-            answers.append(question.answer)
+        for j in 1 ... table {
+            for i in 1 ... 10 {
+                let question = Question(tableNumber: j, multiplier: i)
+                questions.append(question.question)
+                answers.append(question.answer)
+            }
         }
         print("Array of questions: \(questions)")
         print("Answers array: \(answers)")
@@ -109,7 +109,7 @@ struct ContentView: View {
     func asking(questions quantity: Int) {
         
         if quantity > 0 {
-            currentQuestionNumber = Int.random(in: 0 ... (selectedQuestionsLimit - 1))
+            currentQuestionNumber = Int.random(in: 0 ... (table * 10 - 1))
             print("Current question number: \(currentQuestionNumber)")
         } else {
             self.isGameRunning = false
@@ -122,21 +122,13 @@ struct ContentView: View {
         
         print("Answer: \(answer)")
         
-        if answer != "" {
-            if let answerNumber = answers.firstIndex(of: Int(answer)!) {
-                if answerNumber == currentQuestionNumber {
-                    print("Correct!")
-                } else {
-                    print("Wrong!")
-                }
-            } else {
-                print("Something wrong with tables of data")
-                return
-            }
-            asking(questions: remainingQuestionsQuantity)
+        let answerValue = answers[currentQuestionNumber]
+        if answer == answerValue {
+            print("Correct!")
         } else {
-            print("Could not find an answer!")
+            print("Wrong!")
         }
+        asking(questions: remainingQuestionsQuantity)
     }
  }
 
